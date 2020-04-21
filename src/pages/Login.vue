8e9bdcba69b5d069b7e346866b6867cf136334fd
heyu <heyu@qq.com>
2020-04-21 23:06:16 +0800

    2020.4.21 23:05已提交

---
+++ src/pages/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <b-navbar type="dark" variant="dark">
+          <b-navbar-brand href="#">尚筹网-创意产品众筹平台</b-navbar-brand>
+      </b-navbar>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import {NavbarPlugin} from 'bootstrap-vue'
+
+Vue.use(NavbarPlugin);
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
