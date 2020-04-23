b85ccecf48de0d18ade3c5a44032d239758d7862
lenovo <lenovo@qq.com>
2020-04-23 20:01:46 +0800

    2020.4.23 20:01已提交

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -12,19 +12,38 @@
         </b-form-group>
         <b-form-group>
           <b-input-group>
-            <b-form-input required placeholder="请输入登录账号" name="loginacct"></b-form-input>
+            <b-form-input
+              required
+              placeholder="请输入登录账号"
+              v-model="account"
+              :state="validateAccount"
+              @focus="focus('account')"
+              @blur="blur('account')"
+            ></b-form-input>
             <b-input-group-append is-text>
               <b-icon icon="person-fill"></b-icon>
             </b-input-group-append>
           </b-input-group>
+          <b-form-invalid-feedback :state="validateAccount">用户登录账号不能为空，请输入</b-form-invalid-feedback>
+          <b-form-valid-feedback :state="validateAccount">好的</b-form-valid-feedback>
         </b-form-group>
         <b-form-group>
           <b-input-group>
-            <b-form-input type="password" required placeholder="请输入登录密码" name="userpswd"></b-form-input>
+            <b-form-input
+              type="password"
+              required
+              placeholder="请输入登录密码"
+              v-model="password"
+              :state="validatePassword"
+              @focus="focus('password')"
+              @blur="blur('password')"
+            ></b-form-input>
             <b-input-group-append is-text>
               <b-icon icon="lock-fill"></b-icon>
-            </b-input-group-append> 
+            </b-input-group-append>
           </b-input-group>
+          <b-form-invalid-feedback :state="validatePassword">用户登录密码不能为空，请输入</b-form-invalid-feedback>
+          <b-form-valid-feedback :state="validatePassword">好的</b-form-valid-feedback>
         </b-form-group>
         <b-form-group>
           <b-form-select required :options="roles" v-model="food"></b-form-select>
@@ -43,7 +62,7 @@
 </template>
 
 <script>
-import {login} from "../ajax/ajax.js"
+import { login } from "../ajax/ajax.js";
 import Vue from "vue";
 import {
   NavbarPlugin,
@@ -64,18 +83,43 @@ Vue.use(InputGroupPlugin);
 export default {
   data() {
     return {
+      account: "",
+      password: "",
       food: "member",
       roles: [
         { text: "会员", value: "member" },
         { text: "管理", value: "user" }
-      ]
+      ],
+      isfocus: { accountFocus: false, passwordFocus: false }
     };
   },
-  methods:{
-    submit(){
-      login({}).then((response) => {
+  methods: {
+    submit() {
+      login({}).then(response => {
         console.log(response);
       });
+    },
+    focus(type){
+      if (type=="account") {
+        this.isfocus.accountFocus=true;
+      }else if (type=="password") {
+        this.isfocus.passwordFocus=true;
+      }
+    },
+    blur(type){
+      if (type=="account") {
+        this.isfocus.accountFocus=false;
+      }else if (type=="password") {
+        this.isfocus.passwordFocus=false;
+      }
+    }
+  },
+  computed: {
+    validateAccount() {
+      return this.isfocus.accountFocus ?  this.account != "" : null;
+    },
+    validatePassword() {
+      return this.isfocus.passwordFocus ? this.password != "": null;
     }
   }
 };
