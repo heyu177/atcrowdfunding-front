61753bc2e2fd262de54502aca31f84c5f40ee3e0
lenovo <lenovo@qq.com>
2020-04-29 18:43:09 +0800

    2020.4.29 18:43已提交

---
+++ src/pages/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <b-navbar type="dark" variant="dark"></b-navbar>
+  </div>
+</template>
+
+<script>
+import Vue from "vue"
+import {NavPlugin} from "bootstrap-vue"
+
+Vue.use(NavPlugin);
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
