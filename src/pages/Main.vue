5df852df8a60bb30264251d5186248c91cbbee37
lenovo <lenovo@qq.com>
2020-04-30 19:08:31 +0800

    2020.4.30 19:08已提交

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,20 +1,88 @@
 <template>
   <div>
-      <b-navbar type="dark" variant="dark"></b-navbar>
+    <b-navbar type="dark" variant="dark" style="padding:0 1rem">
+      <b-navbar-brand href="#" style="font-size:2rem;color:#9d9d9d">众筹平台 - 控制面板</b-navbar-brand>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-form class="m-2">
+          <b-form-input placeholder="查询"></b-form-input>
+        </b-nav-form>
+        <b-button-group class="m-2">
+          <b-dropdown variant="success">
+            <template v-slot:button-content>
+              <b-icon icon="person-fill"></b-icon>张三
+            </template>
+            <b-dropdown-item>个人设置</b-dropdown-item>
+            <b-dropdown-item>消息</b-dropdown-item>
+            <b-dropdown-divider></b-dropdown-divider>
+            <b-dropdown-item>退出系统</b-dropdown-item>
+          </b-dropdown>
+        </b-button-group>
+        <b-button class="m-2" variant="danger">
+          <b-icon icon="question-circle-fill"></b-icon>帮助
+        </b-button>
+      </b-navbar-nav>
+    </b-navbar>
+    <b-container fluid>
+      <b-row>
+        <b-col sm="3" md="2">
+          <div style="background:#f5f5f5;height:100%"></div>
+        </b-col>
+        <b-col sm="9" md="10">
+          <h1 class="title">控制面板</h1>
+          <b-row>
+            <b-col sm="6" lg="3" style="text-align:center">
+              <b-img rounded="circle" blank="true" blank-color="#0d8fdb" alt="200x200" width="200" height="200"></b-img>
+              <h4>Label</h4>
+              <span>Something else</span>
+            </b-col>
+            <b-col sm="6" lg="3" style="text-align:center">
+              <b-img rounded="circle" blank="true" blank-color="#39dbac" alt="200x200" width="200" height="200"></b-img>
+              <h4>Label</h4>
+              <span>Something else</span>
+            </b-col>
+            <b-col sm="6" lg="3" style="text-align:center">
+              <b-img rounded="circle" blank="true" blank-color="#0d8fdb" alt="200x200" width="200" height="200"></b-img>
+              <h4>Label</h4>
+              <span>Something else</span>
+            </b-col>
+            <b-col sm="6" lg="3" style="text-align:center">
+              <b-img rounded="circle" blank="true" blank-color="#39dbac" alt="200x200" width="200" height="200"></b-img>
+              <h4>Label</h4>
+              <span>Something else</span>
+            </b-col>
+          </b-row>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import Vue from "vue"
-import {NavPlugin} from "bootstrap-vue"
+import Vue from "vue";
+import {
+  NavPlugin,
+  NavbarPlugin,
+  FormInputPlugin,
+  ButtonPlugin,
+  ButtonGroupPlugin,
+  LayoutPlugin,
+  ImagePlugin
+} from "bootstrap-vue";
 
 Vue.use(NavPlugin);
+Vue.use(NavbarPlugin);
+Vue.use(FormInputPlugin);
+Vue.use(ButtonPlugin);
+Vue.use(ButtonGroupPlugin);
+Vue.use(LayoutPlugin);
+Vue.use(ImagePlugin);
 
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
+.title{
+  padding: 1rem;
+  border-bottom: 1px solid #f4f4f4;
+}
 </style>
