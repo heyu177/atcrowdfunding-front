19f5d6edacbddd46af9a2871bfb07e4c0551d623
lenovo <lenovo@qq.com>
2020-05-22 18:22:34 +0800

    2020.5.22 18:22已提交

--- src/pages/permission/PermissionInfo.vue
+++ src/pages/permission/PermissionInfo.vue
@@ -1,19 +1,31 @@
 <template>
-  <div>
-    <ul id="treeDemo" class="ztree"></ul>
-  </div>
+  <b-card no-body class="m-3">
+    <b-card-header>
+      <b-icon icon="grid3x3-gap"></b-icon>数据列表
+    </b-card-header>
+    <b-card-body>
+      <div>
+        <ul id="treeDemo" class="ztree"></ul>
+      </div>
+    </b-card-body>
+  </b-card>
 </template>
 
 <script>
-import {async} from "../../ajax/ajax.js"
+import Vue from "vue";
+import { async } from "../../ajax/ajax.js";
+import { CardPlugin } from "bootstrap-vue";
+
+Vue.use(CardPlugin);
+
 export default {
   data() {
     return {
-      setting:{async}
+      setting: { async }
     };
   },
-  mounted(){
-      $.fn.zTree.init($("#treeDemo"), this.setting);
+  mounted() {
+    $.fn.zTree.init($("#treeDemo"), this.setting);
   }
 };
 </script>
